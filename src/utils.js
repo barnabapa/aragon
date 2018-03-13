27cc8a7987f8a02cc6e74c8f9bd4f18c1030bec1
Brett Sun <qisheng.brett.sun@gmail.com>
2018-03-13 14:51:15 +0100

    Add compose util

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,16 @@
+// Stealing this from recompose / etc for now
+export function compose(...funcs) {
+  if (funcs.length === 0) {
+    return arg => arg
+  }
+
+  if (funcs.length === 1) {
+    return funcs[0]
+  }
+
+  return funcs.reduce((a, b) => (...args) => a(b(...args)))
+}
+
 export function noop() {}
 
 export function makeEtherscanBaseUrl(network) {
